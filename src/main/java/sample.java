26b65a2820353415da8aa658257f0a7c0de33ca5
utham-ace <guvvala.p532@gmail.com>
2025-07-09 12:43:39 +0530

    Update sample.java

--- src/main/java/sample.java
+++ src/main/java/sample.java
@@ -3,5 +3,7 @@ class sample
     public static void main(String... args)
     {
       System.out.println("This is from jar craetion file");
+      System.out.println("This is second line coming from jar craetion file");
+      
     }
   }
